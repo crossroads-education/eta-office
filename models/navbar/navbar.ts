c3b637070880620090b5d31bd7e06edf24a70436
Alex Hicks <aldahick@iupui.edu>
2016-07-17 00:14:33 -0400

    Implement navbar

---
+++ models/navbar/navbar.ts
@@ -0,0 +1,21 @@
+import * as eta from "eta-lib";
+
+import * as express from "express";
+import * as fs from "fs";
+
+export class Model implements eta.Model {
+
+    private params : eta.ModelParams;
+
+    public setParams(params : eta.ModelParams) : void {
+        this.params = params;
+    }
+
+    public render(req : express.Request, res : express.Response, callback : (env : {[key : string] : any}) => void) : void {
+        let navbarData : any = JSON.parse(fs.readFileSync("modules/office/models/navbar.json").toString());
+        let navbarHTML : string = eta.navbar.build(navbarData, req.session["permissions"], this.params.baseUrl, false);
+        callback({
+            "navbarHTML": navbarHTML
+        });
+    }
+}
