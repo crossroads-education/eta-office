ed4a27ff3da83ad8a636d6e008d5219583de5f8b
Alex Hicks <aldahick@iupui.edu>
2016-07-24 13:43:30 -0400

    Implement /post/update-schedule

---
+++ models/post/update-schedule/update-schedule.ts
@@ -0,0 +1,49 @@
+import * as eta from "eta-lib";
+
+import * as express from "express";
+
+export class Model implements eta.Model {
+    public render(req : express.Request, res : express.Response, callback : (env : {[key : string] : any}) => void) : void {
+        if (!eta.params.test(req.body, ["day", "term", "cells"])) {
+            callback({errcode: eta.http.InvalidParameters});
+            return;
+        }
+        let moreSql : string = "WHEN id = ? AND time = ? THEN ?\n";
+        let params : string[] = [];
+        let cells : any[] = JSON.parse(req.body.cells);
+        eta.center.getAll(function(centers : eta.Center[]) {
+            for (let i : number = 0; i < cells.length; i++) {
+                let center : number = -1;
+                for (let k : number = 0; k < centers.length; k++) {
+                    if (centers[k].code == cells[i].centerCode) {
+                        center = centers[k].id;
+                        break;
+                    }
+                }
+                params = params.concat([cells[i].userid, cells[i].time, center]);
+            }
+            let sql : string = `
+                UPDATE
+                    EmployeeSchedule
+                SET
+                    center = (CASE
+                        ${moreSql.repeat(params.length / 3)}
+                        ELSE EmployeeSchedule.center
+                    END)
+                WHERE day = ? AND term = ?
+            `;
+            params.push(req.body.day);
+            params.push(req.body.term);
+            eta.logger.trace(sql);
+            eta.logger.json(params);
+            eta.db.query(sql, params, (err : eta.DBError, rows : any[]) => {
+                if (err) {
+                    eta.logger.dbError(err);
+                    callback({errcode: eta.http.InternalError});
+                    return;
+                }
+                callback({raw: "true"});
+            });
+        });
+    }
+}
