18664901804d9daf8b4336fdd3b553c9542a4ccc
Alex Hicks <aldahick@iupui.edu>
2016-08-18 11:18:57 -0400

    Implement /log/dev

---
+++ models/log/dev/dev.ts
@@ -0,0 +1,19 @@
+import * as eta from "eta-lib";
+
+import * as express from "express";
+import * as git from "simple-git";
+
+export class Model implements eta.Model {
+    public render(req : express.Request, res : express.Response, callback : (env : {[key : string] : any}) => void) : void {
+        git("modules/office").log(function(err : Error, log : {all : any[]}) {
+            if (err) {
+                eta.logger.warn("Could not get Git history: " + err.message);
+                callback({errcode: eta.http.InternalError});
+                return;
+            }
+            callback({
+                "commits": log.all
+            });
+        });
+    }
+}
---
+++ static/js/log/dev.ts
@@ -0,0 +1,19 @@
+import "datatables.net";
+import "datatables.net-bs";
+
+export module dev {
+    function setupRow($row : JQuery) : void {
+        let message : string = $row.find(".cell-message").html();
+        message = message.replace(/\#([\d]+)/g, "<a href='https://github.com/crossroads-education/eta-office/issues/$1'>#$1</a>");
+        $row.find(".cell-message").html(message);
+    }
+
+    $(document).ready(function() {
+        $("#table-commits tbody tr").each(function(index : number, element : HTMLElement) {
+            setupRow($(element));
+        });
+        $("#table-commits").dataTable(<any>{
+            "order": [[0, "desc"]]
+        });
+    });
+}
