16d40d702bddd48bbbcebe9852c43093a646bd4d
Alex Hicks <aldahick@iupui.edu>
2016-08-22 20:53:30 -0400

    Fix time column in /log/system

--- static/js/log/system.ts
+++ static/js/log/system.ts
@@ -4,9 +4,20 @@ import "datatables.net-buttons";
 import "datatables.net-buttons-bs";
 import "datatables.net-buttons-html5";
 import "datatables.net-buttons-print";
+import * as moment from "moment";
 
 export module system {
+
+    function fixTimeCell() {
+        let $this : JQuery = $(this);
+        let time : string = moment($this.text(), ["h:mm:ss A"]).format("HH:mm:ss");
+        $this.text(time);
+    }
+
     $(document).ready(function() {
-        $("#table-log").dataTable();
+        $("#table-log .cell-time").each(fixTimeCell);
+        $("#table-log").dataTable(<any>{
+            "order": [[0, "desc"]]
+        });
     });
 }
