fcb0b9594d95d1c9a120386af98711e2ef19a841
Brian Huntley <bboy300@comcast.net>
2016-11-16 14:25:53 -0500

    Implement weekly timesheet seperation in /templates/timesheet

--- static/js/templates/timesheet.ts
+++ static/js/templates/timesheet.ts
@@ -30,10 +30,30 @@ export module timesheet {
         table.fnAddData(data, true);
     }
 
+    function getWeekOfTheYear(date : Date = new Date){
+        date = new Date(date.getTime());
+        date.setHours(0, 0, 0, 0);
+        date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+        let week1 = new Date(date.getFullYear(), 0, 4);
+        return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+        //algorithm from https://weeknumber.net/how-to/javascript
+    }
+    export function updateRowBorders(): void{
+        for(let i : number = 0; i < rows.length - 1; i++){
+            let date2 : Date = new Date(rows[i+1].children[0].innerHTML);
+            if(getWeekOfTheYear(date1) != getWeekOfTheYear(date2)){
+                $(rows[i + 1].children).css("border-top-width", "5px")
+                    .css("border-top-color", "#444651");
+            }
+        }
+    }
     $(document).ready(function() {
         table = $("#table-timesheet").dataTable(<any>{
             "order": [[0, "desc"], [1, "desc"]]
         });
+        $("#table-timesheet").on("draw.dt", function () {
+            updateRowBorders();
+        })
         useIP = $("#table-timesheet").data("use-ip");
     });
 }
