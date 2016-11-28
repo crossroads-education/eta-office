304f1b8e446af862ffff3df18fab42133d33c38c
Alex Hicks <aldahick@iupui.edu>
2016-11-28 16:44:58 -0500

    Add 'Enrollment Count' column to deviation report

--- lib/reports/deviation.sql
+++ lib/reports/deviation.sql
@@ -3,7 +3,8 @@ SELECT
     `Term`.`name`,
     COUNT(DISTINCT `Visit`.`student`, `Visit`.`timeIn`) AS "Visit Count",
     MAX(`CurrentVisits`.`count`) AS "Selected Term",
-    COUNT(DISTINCT `Visit`.`student`, `Visit`.`timeIn`) - MAX(`CurrentVisits`.`count`) AS "Divergence From Selected Term"
+    COUNT(DISTINCT `Visit`.`student`, `Visit`.`timeIn`) - MAX(`CurrentVisits`.`count`) AS "Divergence From Selected Term",
+    MAX(`EnrollmentCount`.`count`) AS "Enrollment Count"
 FROM
     `Visit`
         LEFT JOIN `Term` ON
@@ -28,6 +29,25 @@ FROM
                 `Visit`.`term`
         ) AS `CurrentVisits` ON
             1
+        LEFT JOIN (
+            SELECT
+                `Course`.`center`,
+                `Section`.`term`,
+                COUNT(DISTINCT `StudentSection`.`student`, `StudentSection`.`section`) AS "count"
+            FROM
+                `StudentSection`
+                    LEFT JOIN `Section` ON
+                        `StudentSection`.`section` = `Section`.`id`
+                    LEFT JOIN `Course` ON
+                        `Section`.`course` = `Course`.`id`
+            WHERE
+                `StudentSection`.`status` = 'E'
+            GROUP BY
+                `Course`.`center`,
+                `Section`.`term`
+        ) AS `EnrollmentCount` ON
+            `Visit`.`center` = `EnrollmentCount`.`center` AND
+            `Visit`.`term` = `EnrollmentCount`.`term`
 WHERE
     `CurrentVisits`.`term` = `SelectedTerm`.`id` AND
     (`Term`.`term` % 10) = (`SelectedTerm`.`term` % 10) AND
