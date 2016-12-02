8407c59c45b916668f1c514f24add9a93603b94f
Alex Hicks <aldahick@iupui.edu>
2016-12-02 13:18:35 -0500

    Finish visits-center report

--- lib/reports/visits-center.sql
+++ lib/reports/visits-center.sql
@@ -1,17 +1,44 @@
 -- center,term
 SELECT
-    CONCAT(`Person`.`firstName`, ' ', `Person`.`lastName`) AS "Student Name",
-    `Person`.`username` AS "Student Username",
-    COUNT(DISTINCT `Visit`.`student`, `Visit`.`timeIn`) AS "Visit Count"
+    `Person`.`lastName` AS "Last Name",
+    `Person`.`firstName` AS "First Name",
+    -- CONCAT(`Person`.`lastName`, ' ', `Person`.`firstName`) AS "Student Name",
+    `Person`.`username` AS "Username",
+    GROUP_CONCAT(DISTINCT CONCAT(`Course`.`subject`, ' ', `Course`.`number`, ' ', `Section`.`number`) ORDER BY `Course`.`subject`, `Course`.`number`, `Section`.`number`) AS "Sections",
+    `VisitTotals`.`count` AS "Visit Count",
+    IFNULL(
+        `VisitTotals`.`duration`,
+        "0.00"
+    ) AS "Total Visit Duration (Hours)"
 FROM
-    `Visit`
+    (
+        SELECT
+            `Visit`.`student`,
+            `Visit`.`center`,
+            `Visit`.`term`,
+            COUNT(DISTINCT `Visit`.`student`, `Visit`.`timeIn`) AS "count",
+            ROUND(SUM(TIME_TO_SEC(TIMEDIFF(`Visit`.`timeOut`, `Visit`.`timeIn`))) / 3600, 2) AS "duration"
+        FROM
+            `Visit`
+        WHERE
+            `Visit`.`center` = ? AND
+            `Visit`.`term` = ?
+        GROUP BY
+            `Visit`.`student`
+    ) AS `VisitTotals`
         LEFT JOIN `Person` ON
-            `Visit`.`student` = `Person`.`id`
+            `VisitTotals`.`student` = `Person`.`id`
+        RIGHT JOIN `StudentSection` ON
+            `VisitTotals`.`student` = `StudentSection`.`student`
+        RIGHT JOIN `Section` ON
+            `StudentSection`.`section` = `Section`.`id`
+        RIGHT JOIN `Course` ON
+            `Section`.`course` = `Course`.`id`
 WHERE
-    `Visit`.`center` = ? AND
-    `Visit`.`term` = ?
+    `Section`.`term` = `VisitTotals`.`term` AND
+    `Course`.`center` = `VisitTotals`.`center`
 GROUP BY
-    `Visit`.`student`
+    `VisitTotals`.`student`
 ORDER BY
-    `Person`.`firstName`,
-    `Person`.`lastName`
+    `Person`.`lastName`,
+    `Person`.`firstName`
