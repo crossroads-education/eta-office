f1150fb6f59e7885b1e58917f1e4a04bb8cc7bc5
Alex Hicks <aldahick@iupui.edu>
2017-01-24 11:05:19 -0500

    Clean up sudo page a bit

--- models/post/set-sudo/set-sudo.ts
+++ models/post/set-sudo/set-sudo.ts
@@ -4,13 +4,12 @@ import * as express from "express";
 
 export class Model implements eta.Model {
     public render(req: express.Request, res: express.Response, callback: (env: { [key: string]: any }) => void): void {
-        eta.logger.json(req.body);
         if (!eta.params.test(req.body, ["username"])) {
             return callback({
-                errcode : eta.http.InvalidParameters
+                errcode: eta.http.InvalidParameters
             });
         } else {
-            eta.login.login(req.body.username, req, (env : { [key: string]: any }) => {
+            eta.login.login(req.body.username, req, (env: { [key: string]: any }) => {
                 req.session["returnTo"] = "/office/manager/sudo";
                 eta.redirect.back(req, res);
             });
