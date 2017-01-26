f62441612fe39555d0ea4447aa12c6f5adfede78
Kroeker <nkroeker@iupui.edu>
2017-01-26 14:22:00 -0500

    Implement design views for policies page

---
+++ static/js/policies.ts
@@ -0,0 +1,11 @@
+import "templates/modal";
+
+export module policies {
+    // $(document).ready(function() {
+    //     $(".accordion").accordion({
+    //         "heightStyle": "content",
+    //         "collapsible": true,
+    //         "active": false
+    //     });
+    // });
+}
