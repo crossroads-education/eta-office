c07f14fb3edc85ded198ef42f7181c0a39b01763
Gaby Justice <gnjustic@iupui.edu>
2018-05-09 15:49:06 -0400

    make everything work off of current term

--- models/manager/centers/centers.ts
+++ models/manager/centers/centers.ts
@@ -53,7 +53,7 @@ export class Model implements eta.Model {
                     centers[i].name = eta.time.daysOfWeek[rows[i].day];
                 }
             }
-            let terms: eta.Term[] = eta.term.getClosest(eta.term.get(req.query.term), true);
+            let terms: eta.Term[] = eta.term.getClosest(eta.term.get(req.query.term));
             callback({
                 "currentCenter": req.query.center,
                 "currentTerm": req.query.term,
